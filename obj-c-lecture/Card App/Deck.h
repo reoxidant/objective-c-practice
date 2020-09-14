da1d86d121741d2fe24a4cf739c226277507d86b
Vitaliy Shapovalov <kupper133@gmail.com>
2020-09-14 23:39:14 +0300

    add new structure for files

---
+++ obj-c-lecture/Card App/Deck.h
@@ -0,0 +1,17 @@
+//
+//  Deck.h
+//  obj-c-lecture
+//
+//  Created by Виталий Шаповалов on 14.09.2020.
+//  Copyright © 2020 Виталий Шаповалов. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Deck : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ obj-c-lecture/Card App/Deck.m
@@ -0,0 +1,13 @@
+//
+//  Deck.m
+//  obj-c-lecture
+//
+//  Created by Виталий Шаповалов on 14.09.2020.
+//  Copyright © 2020 Виталий Шаповалов. All rights reserved.
+//
+
+#import "Deck.h"
+
+@implementation Deck
+
+@end
